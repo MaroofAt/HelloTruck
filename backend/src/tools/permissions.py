e773acc9796a452afbf1f6ce0f6268c75c86dd22
Ali-Almusfi <alialmsfi09@gmail.com>
2026-06-26 18:28:51 +0300

    Add permissions for (Captain , Trader , Admin , Sub_Admin) in tools folder

---
+++ backend/src/tools/permissions.py
@@ -0,0 +1,33 @@
+from rest_framework.permissions import BasePermission
+from .models import Credential  
+
+class IsCaptain (BasePermission):
+    def has_permission(self, request, *args):
+        if not request.user or not request.user.is_authenticated:
+            return False
+        
+        return request.user.role == Credential.ROLE.CAPTAIN
+    
+
+class IsTrader (BasePermission):
+     def has_permission(self, request, **args):
+         if not request.user or not request.user.is_authenticated:
+             return False
+         
+         return request.user.role == Credential.Role.TRADER
+     
+
+class IsAdmin (BasePermission):
+     def has_permission(self, request, **args):
+         if not request.user or not request.user.is_authenticated:
+             return False
+         
+         return request.user.role == Credential.Role.ADMIN
+     
+
+class IsSubAdmin (BasePermission):
+     def has_permission(self, request, **args):
+         if not request.user or not request.user.is_authenticated:
+             return False
+         
+         return request.user.role == Credential.Role.SUB_ADMIN
