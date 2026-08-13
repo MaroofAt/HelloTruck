398f52a34083f428c4feab529e16f394f231b78d
Ali-Almusfi <alialmsfi09@gmail.com>
2026-08-13 18:58:35 +0300

    fix the list branches representation

--- backend/src/dashboard/serializers.py
+++ backend/src/dashboard/serializers.py
@@ -5,8 +5,8 @@ from .models import Branch , Location
 
 class BranchSerializer(serializers.ModelSerializer):
 
-    longitude = serializers.FloatField(write_only=True)
-    latitude = serializers.FloatField(write_only = True)
+    longitude = serializers.FloatField(required=False)
+    latitude = serializers.FloatField(required=False)
 
     class Meta:
         model = Branch
@@ -22,10 +22,27 @@ class BranchSerializer(serializers.ModelSerializer):
             'location': {'required': False}
         }
 
+    def to_representation(self, instance):
+        
+        ret = super().to_representation(instance)
+        if instance.location:
+            ret['longitude'] = float(instance.location.longitude)
+            ret['latitude'] = float(instance.location.latitude)
+        else:
+            ret['longitude'] = None
+            ret['latitude'] = None
+        return ret
+
     def create (self, validated_data):
 
-        longitude = validated_data.pop("longitude")
-        latitude = validated_data.pop("latitude")
+        longitude = validated_data.pop("longitude" , None)
+        latitude = validated_data.pop("latitude", None)
+        # print(longitude)
+        # print(latitude)
+        if longitude is None or latitude is None:
+            raise serializers.ValidationError({
+                'non_field_errors': 'Both latitude and longitude are required to create a branch.'
+            })
 
         location , created = Location.objects.get_or_create(
             latitude=latitude, longitude=longitude, 
